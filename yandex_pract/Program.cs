e9061aab0e6cff460902fb563c139363c8c5dd3a
rizorpolder <rizorpolder@gmail.com>
2026-03-29 12:53:39 +0300

    added empty proj

---
+++ yandex_pract/Program.cs
@@ -0,0 +1,24 @@
+namespace yandex_pract;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+
+		builder.Services.AddControllers();
+
+		var app = builder.Build();
+
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+
+		app.MapControllers();
+
+		app.Run();
+	}
+}
